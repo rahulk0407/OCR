8cec5cf2d3bbb98c6759121fb99cd1a0324b82ed
rahul kumar <47654620+rahulk0407@users.noreply.github.com>
2019-06-17 18:58:16 +0530

    Add files via upload

---
+++ card_reader/data_production.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 10:41:06 2019
+
+@author: rahul
+"""
+
+import os
+
+def makedir(directory):
+    """Creates a new directory if it does not exist"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        return None, 0
+    
+for i in range(0,10):
+    directory_name = "./credit_card/train/"+str(i)
+    print(directory_name)
+    makedir(directory_name) 
+
+for i in range(0,10):
+    directory_name = "./credit_card/test/"+str(i)
+    print(directory_name)
+    makedir(directory_name)
+    
+import cv2
+import numpy as np 
+import random
+import cv2
+from scipy.ndimage import convolve
+
+def DigitAugmentation(frame, dim = 32):
+    """Randomly alters the image using noise, pixelation and streching image functions"""
+    frame = cv2.resize(frame, None, fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
+    frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+    random_num = np.random.randint(0,9)
+
+    if (random_num % 2 == 0):
+        frame = add_noise(frame)
+    if(random_num % 3 == 0):
+        frame = pixelate(frame)
+    if(random_num % 2 == 0):
+        frame = stretch(frame)
+    frame = cv2.resize(frame, (dim, dim), interpolation = cv2.INTER_AREA)
+
+    return frame 
+
+def add_noise(image):
+    """Addings noise to image"""
+    prob = random.uniform(0.01, 0.05)
+    rnd = np.random.rand(image.shape[0], image.shape[1])
+    noisy = image.copy()
+    noisy[rnd < prob] = 0
+    noisy[rnd > 1 - prob] = 1
+    return noisy
+
+def pixelate(image):
+    "Pixelates an image by reducing the resolution then upscaling it"
+    dim = np.random.randint(8,12)
+    image = cv2.resize(image, (dim, dim), interpolation = cv2.INTER_AREA)
+    image = cv2.resize(image, (16, 16), interpolation = cv2.INTER_AREA)
+    return image
+
+def stretch(image):
+    "Randomly applies different degrees of stretch to image"
+    ran = np.random.randint(0,3)*2
+    if np.random.randint(0,2) == 0:
+        frame = cv2.resize(image, (32, ran+32), interpolation = cv2.INTER_AREA)
+        return frame[int(ran/2):int(ran+32)-int(ran/2), 0:32]
+    else:
+        frame = cv2.resize(image, (ran+32, 32), interpolation = cv2.INTER_AREA)
+        return frame[0:32, int(ran/2):int(ran+32)-int(ran/2)]
+    
+def pre_process(image, inv = False):
+    """Uses OTSU binarization on an image"""
+    try:
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    except:
+        gray_image = image
+        pass
+    
+    if inv == False:
+        _, th2 = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    else:
+        _, th2 = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    resized = cv2.resize(th2, (32,32), interpolation = cv2.INTER_AREA)
+    return resized
+
+
+cc1 = cv2.imread('creditcard_digits2.jpg', 0)
+_, th2 = cv2.threshold(cc1, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+cv2.imshow("cc1", th2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#using crop_and_x_y_cordinates_detection.py
+region = [(0, 0), (35, 48)]
+
+# Assigns values to each region for ease of interpretation
+top_left_y = region[0][1]
+bottom_right_y = region[1][1]
+top_left_x = region[0][0]
+bottom_right_x = region[1][0]
+
+for i in range(0,1): #We only look at the first digit in testing out augmentation functions
+    roi = cc1[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+    for j in range(0,10):
+        roi2 = DigitAugmentation(roi)
+        roi_otsu = pre_process(roi2, inv = False)
+        cv2.imshow("otsu", roi_otsu)
+        cv2.waitKey(0)
+        
+cv2.destroyAllWindows()
+
+
+cc1 = cv2.imread('creditcard_digits1.jpg', 0)
+
+_, th2 = cv2.threshold(cc1, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+cv2.imshow("cc1", th2)
+cv2.imshow("creditcard_digits1", cc1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+region = [(2, 19), (50, 72)]
+
+top_left_y = region[0][1]
+bottom_right_y = region[1][1]
+top_left_x = region[0][0]
+bottom_right_x = region[1][0]
+
+for i in range(0,10):   
+    # We jump the next digit each time we loop
+    if i > 0:
+        top_left_x = top_left_x + 59
+        bottom_right_x = bottom_right_x + 59
+
+    roi = cc1[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+    print("Augmenting Digit - ", str(i))
+    # We create 1000 versions of each image for our dataset
+    for j in range(0,1000):
+        roi2 = DigitAugmentation(roi)
+        roi_otsu = pre_process(roi2, inv = True)
+        cv2.imwrite("./credit_card/train/"+str(i)+"./_1_"+str(j)+".jpg", roi_otsu)
+cv2.destroyAllWindows()
+
+
+
+# Creating 2000 Images for each digit in creditcard_digits2 - TRAINING DATA
+
+cc1 = cv2.imread('creditcard_digits2.jpg', 0)
+_, th2 = cv2.threshold(cc1, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+cv2.imshow("cc1", th2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+region = [(0, 0), (35, 48)]
+
+top_left_y = region[0][1]
+bottom_right_y = region[1][1]
+top_left_x = region[0][0]
+bottom_right_x = region[1][0]
+
+for i in range(0,10):   
+    if i > 0:
+        # We jump the next digit each time we loop
+        top_left_x = top_left_x + 35
+        bottom_right_x = bottom_right_x + 35
+
+    roi = cc1[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+    print("Augmenting Digit - ", str(i))
+    # We create 1000 versions of each image for our dataset
+    for j in range(0,1000):
+        roi2 = DigitAugmentation(roi)
+        roi_otsu = pre_process(roi2, inv = False)
+        cv2.imwrite("./credit_card/train/"+str(i)+"./_2_"+str(j)+".jpg", roi_otsu)
+        #cv2.imshow("otsu", roi_otsu)
+
+cv2.destroyAllWindows()
